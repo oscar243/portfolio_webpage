0ec4374dc378adc190b8bf5f8db9b290e9f5fed2
Oscar Virguez <oscarandres243@outlook.com>
2023-02-27 20:38:24 -0500

    SMTP gmail

---
+++ test/script.js
@@ -0,0 +1,6 @@
+var colorBtn = document.getElementById("color-btn");
+
+// Cambiar el color de fondo al hacer clic en el botón
+colorBtn.addEventListener("click", function() {
+  document.body.style.backgroundColor = "red";
+});
